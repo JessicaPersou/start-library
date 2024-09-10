84a1f305a728b16d1b6ac887d5810d9943e0a18b
Jessica Sousa <jessica.sousa@koin.com.br>
2024-09-10 10:10:54 -0300

    first commit

---
+++ src/main/java/br/com/persou/startlibrary/StartLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.persou.startlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/persou/startlibrary/StartLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.persou.startlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
